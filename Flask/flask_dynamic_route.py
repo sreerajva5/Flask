b41c8dfbd4e07ceef9af9cb141f038a42abeff35
sreerajva5 <39976669+sreerajva5@users.noreply.github.com>
2020-10-13 08:24:30 +0530

    dynamic routing
    flask - dynamic routing

---
+++ Flask/flask_dynamic_route.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>Hello Puppy!</h1>"
+
+
+
+
+@app.route('/information')
+def info():
+    return "<h1>Puppies are cute</h1>"
+
+
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    return f"<h1>This is a page for {name}</h1>"
+
+
+
+if __name__ == '__main__':
+    app.run()

74abd7b9329fcd6f6c6b305525e73cee38de3321
sreerajva5 <39976669+sreerajva5@users.noreply.github.com>
2020-10-28 21:29:23 +0530

    Template variable (Jinja templating)
    Template variable (Jinja templating):
    This helps to to take variables defined in python directly in html.
    Flask course: Udemy

---
+++ Flask/template_variable/template2.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = 'Sreeraj'
+    letters = list(name)
+    dict1 = {'pup' : 'Sammy'}
+    return render_template('basic.html', name = name, letters = letters, dict1=dict1)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

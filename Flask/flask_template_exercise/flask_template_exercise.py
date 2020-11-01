784b792871c399c8deaae9c09be86778ffe6a7c0
sreerajva5 <39976669+sreerajva5@users.noreply.github.com>
2020-11-01 12:48:42 +0530

    Flask template (with form) exercise
    Flask template (with form) exercise
    
    Flask Course : Udemy

---
+++ Flask/flask_template_exercise/flask_template_exercise.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+@app.route('/report')
+def report():
+    lower_letter = False
+    upper_letter = False
+    num_end = False
+
+    username = request.args.get('username')
+
+    lower_letter = any(c.islower() for c in username)
+    upper_letter = any(c.isupper() for c in username)
+    num_end = username[-1].isdigit()
+
+    report = lower_letter and upper_letter and num_end
+
+    return render_template('report.html', report = report, lower = lower_letter, upper = upper_letter, num_end = num_end)
+
+
+
+
+
+#@app.errorhandler(404)
+#def page_not_found(e):
+#    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
